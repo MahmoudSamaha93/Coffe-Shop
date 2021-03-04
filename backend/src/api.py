0366e980dd5ee9de3eaf0151bf399ef5fba0a3f7
Mahmoud Samaha <samaha.mahmoud@yahoo.com>
2021-03-04 11:08:19 +0200

    hh

--- backend/src/api.py
+++ backend/src/api.py
@@ -31,9 +31,12 @@ db_drop_and_create_all()
 
 @app.route('/drinks', methods=['GET'])
 @requires_auth
-def drinks(payload):
-    print(payload)
-    return ('200', jsonify({"success": True, "drinks": drinks}))
+def get_drinks():
+    drinks = Drink.query.all()
+    short_drinks = [drink.short() for drink in drinks]
+
+    return jsonify({"success": True, "drinks": short_drinks})
+
 
 
 '''
@@ -47,10 +50,12 @@ def drinks(payload):
 
 
 @app.route('/drinks-detail', methods=['GET'])
-@requires_auth(permission='get:drinks-detail')
-def drinks(payload):
-    print(payload)
-    return ('200', jsonify({"success": True, "drinks": drinks}))
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(payload):
+    drinks = Drink.query.all()
+    long_drinks = [drink.long() for drink in drinks]
+
+    return jsonify({"success": True, "drinks": long_drinks})
 
 
 '''
@@ -65,10 +70,32 @@ def drinks(payload):
 
 
 @app.route('/drinks', methods=['POST'])
-@requires_auth(permission='post:drinks')
-def drinks(payload):
-    print(payload)
-    return ('200', jsonify({"success": True, "drinks": drinks}))
+@requires_auth('post:drinks')
+def post_drinks(payload):
+
+    error = False
+    try:
+        # get new values
+        request_json = request.get_json()
+        title = request_json['title']
+        recipe = json.dumps(request_json['recipe'])
+
+        # create a new row in the drinks table
+        drink = Drink(title=title, recipe=recipe)
+        drink.insert()
+        long_drink = drink.long()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        # on successful db insert
+        return jsonify({"success": True, "drinks": long_drink})
+
 
 
 '''
@@ -84,11 +111,38 @@ def drinks(payload):
 '''
 
 
-@app.route('/drinks/<id>', methods=['UPDATE'])
-@requires_auth(permission='patch:drinks')
-def drinks(payload):
-    print(payload)
-    return ('200', jsonify({"success": True, "drinks": drinks}))
+@app.route('/drinks/<id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def patch_drinks(payload, id):
+    success = False
+    try:
+        # get a drink object corresponding to given id
+        drink = Drink.query.get(id)
+
+        # get new values
+        request_json = request.get_json()
+        if 'title' in request_json:
+            # update field values
+            title = request_json['title']
+            drink.title = title
+
+        if 'recipe' in request_json:
+            # update field values
+            recipe = json.dumps(request_json['recipe'])
+            drink.recipe = recipe
+
+        drink.update()
+
+        # mark success
+        success = True
+        long_drink = drink.long()
+    except:
+        db.session.rollback()
+        abort(404)  # it should respond with a 404 error if <id> is not found
+    finally:
+        db.session.close()
+
+    return jsonify({"success": success, "drinks": [long_drink]})
 
 
 '''
@@ -104,10 +158,20 @@ def drinks(payload):
 
 
 @app.route('/drinks/<id>', methods=['DELETE'])
-@requires_auth(permission='patch:drinks')
-def drinks(payload):
-    print(payload)
-    return ('200', jsonify({"success": True, "drinks": drinks}))
+@requires_auth('delete:drinks')
+def delete_drinks(payload, id):
+    success = False
+    try:
+        drink = Drink.query.get(id)
+        drink.delete()
+        success = True
+    except:
+        db.session.rollback()
+        abort(404)  # it should respond with a 404 error if <id> is not found
+    finally:
+        db.session.close()
+
+    return jsonify({"success": success, "delete": id})
 
 
 # Error Handling
@@ -150,7 +214,33 @@ def notfound(error):
         "message": "resource not found"
     }), 404
 
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+    }), 400
 
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify({
+        "success": False,
+        "error": 405,
+        "message": "method not allowed"
+    }), 405
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "internal server error"
+    }), 500
+    
+    
 '''
 @DONE implement error handler for AuthError
     error handler should conform to general task above 
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'mhs-cshop.eu.auth0.com', // the auth0 domain prefix
+    url: 'mhs-cshop', // the auth0 domain prefix
     audience: 'coffe', // the audience set for the auth0 app
     clientId: 'pLfeSeYtPCLxL2sh7io1BdlIMB2WeCH8', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
